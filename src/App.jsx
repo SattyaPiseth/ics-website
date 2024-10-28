c84fbd676ae8aca07024bd6e1bf0ab6c277ab986
piseth sattya <pisethsattya33@gmail.com>
2024-10-28 14:42:10 +0700

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import BlogComponent from "./components/BlogComponent";
 import HeroSection from "./components/HeroSection";
+import Testimonial from "./components/Testimonial";
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
     <>
       <HeroSection />
       <BlogComponent />
+      <Testimonial/>
     </>
   );
 }
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,6 +1,8 @@
 import React, { useCallback, useRef } from "react";
 import "swiper/css";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import testimonialsData from "./../json/layouts/testimonial.json";
 
 const Testimonial = () => {
   const sliderRef = useRef(null);
@@ -16,74 +18,67 @@ const Testimonial = () => {
   }, []);
 
   return (
-    <>
-      <section className="pb-20 pt-20 dark:bg-dark lg:pb-[120px] lg:pt-[120px]">
-        <div className="container mx-auto">
-          <Swiper slidesPerView={1} ref={sliderRef}>
-            <SwiperSlide>
-              <SingleTestimonial
-                image="https://cdn.tailgrids.com/2.0/image/marketing/images/testimonials/testimonial-01/image-01.jpg"
-                reviewImg="https://cdn.tailgrids.com/2.0/image/marketing/images/testimonials/testimonial-01/lineicon.svg"
-                reviewAlt="lineicon"
-                details="Velit est sit voluptas eum sapiente omnis! Porro, impedit minus quam reprehenderit tempore sint quaerat id! Mollitia perspiciatis est asperiores commodi labore!"
-                name="Larry Diamond"
-                position="Chief Executive Officer."
-              />
+    <section className="pb-20 pt-20 dark:bg-dark lg:pb-[120px] lg:pt-[120px]">
+      <div className="container mx-auto">
+        <Swiper
+          slidesPerView={1}
+          ref={sliderRef}
+          modules={[Autoplay]}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+        >
+          {testimonialsData.map((testimonial, index) => (
+            <SwiperSlide key={index}>
+              <SingleTestimonial {...testimonial} />
             </SwiperSlide>
-            <SwiperSlide>
-              <SingleTestimonial
-                image="https://cdn.tailgrids.com/2.0/image/marketing/images/testimonials/testimonial-01/image-01.jpg"
-                reviewImg="https://cdn.tailgrids.com/2.0/image/marketing/images/testimonials/testimonial-01/lineicon.svg"
-                reviewAlt="lineicon"
-                details="Velit est sit voluptas eum sapiente omnis! Porro, impedit minus quam reprehenderit tempore sint quaerat id! Mollitia perspiciatis est asperiores commodi labore!"
-                name="Larry Diamond"
-                position="Chief Executive Officer."
+          ))}
+        </Swiper>
+        <div className="absolute left-0 right-0 z-10 flex items-center justify-center gap-5 sm:bottom-0">
+          <button
+            className="prev-arrow cursor-pointer flex h-14 w-14 items-center justify-center rounded-full border border-stroke bg-white text-dark transition-all hover:border-transparent hover:drop-shadow-testimonial dark:border-dark-3 dark:bg-dark-2 dark:text-white dark:hover:drop-shadow-none"
+            onClick={handlePrev}
+          >
+            {/* SVG Icon for Previous */}
+            <svg
+              width="20"
+              height="21"
+              viewBox="0 0 20 21"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="fill-current"
+            >
+              <path
+                d="M17.5 9.5H4.15625L9.46875 4.09375C9.75 3.8125 9.75 3.375 9.46875 3.09375C9.1875 2.8125 8.75 2.8125 8.46875 3.09375L2 9.65625C1.71875 9.9375 1.71875 10.375 2 10.6562L8.46875 17.2188C8.59375 17.3438 8.78125 17.4375 8.96875 17.4375C9.15625 17.4375 9.3125 17.375 9.46875 17.25C9.75 16.9687 9.75 16.5313 9.46875 16.25L4.1875 10.9062H17.5C17.875 10.9062 18.1875 10.5937 18.1875 10.2187C18.1875 9.8125 17.875 9.5 17.5 9.5Z"
+                fill=""
               />
-            </SwiperSlide>
-            <div className="absolute left-0 right-0 z-10 flex items-center justify-center gap-5 sm:bottom-0">
-              <div className="prev-arrow cursor-pointer" onClick={handlePrev}>
-                <button className="d flex h-[60px] w-[60px] items-center justify-center rounded-full border border-stroke bg-white text-dark transition-all hover:border-transparent hover:drop-shadow-testimonial dark:border-dark-3 dark:bg-dark-2 dark:text-white dark:hover:drop-shadow-none">
-                  <svg
-                    width="20"
-                    height="21"
-                    viewBox="0 0 20 21"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="fill-current"
-                  >
-                    <path
-                      d="M17.5 9.5H4.15625L9.46875 4.09375C9.75 3.8125 9.75 3.375 9.46875 3.09375C9.1875 2.8125 8.75 2.8125 8.46875 3.09375L2 9.65625C1.71875 9.9375 1.71875 10.375 2 10.6562L8.46875 17.2188C8.59375 17.3438 8.78125 17.4375 8.96875 17.4375C9.15625 17.4375 9.3125 17.375 9.46875 17.25C9.75 16.9687 9.75 16.5313 9.46875 16.25L4.1875 10.9062H17.5C17.875 10.9062 18.1875 10.5937 18.1875 10.2187C18.1875 9.8125 17.875 9.5 17.5 9.5Z"
-                      fill=""
-                    />
-                  </svg>
-                </button>
-              </div>
-              <div className="next-arrow cursor-pointer" onClick={handleNext}>
-                <button className="d flex h-[60px] w-[60px] items-center justify-center rounded-full border border-stroke bg-white text-dark transition-all hover:border-transparent hover:drop-shadow-testimonial dark:border-dark-3 dark:bg-dark-2 dark:text-white dark:hover:drop-shadow-none">
-                  <svg
-                    width="20"
-                    height="21"
-                    viewBox="0 0 20 21"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="fill-current"
-                  >
-                    <path
-                      d="M18 9.6875L11.5312 3.125C11.25 2.84375 10.8125 2.84375 10.5312 3.125C10.25 3.40625 10.25 3.84375 10.5312 4.125L15.7812 9.46875H2.5C2.125 9.46875 1.8125 9.78125 1.8125 10.1562C1.8125 10.5312 2.125 10.875 2.5 10.875H15.8437L10.5312 16.2813C10.25 16.5625 10.25 17 10.5312 17.2813C10.6562 17.4063 10.8437 17.4688 11.0312 17.4688C11.2187 17.4688 11.4062 17.4062 11.5312 17.25L18 10.6875C18.2812 10.4062 18.2812 9.96875 18 9.6875Z"
-                      fill=""
-                    />
-                  </svg>
-                </button>
-              </div>
-            </div>
-          </Swiper>
+            </svg>
+          </button>
+          <button
+            className="next-arrow cursor-pointer flex h-14 w-14 items-center justify-center rounded-full border border-stroke bg-white text-dark transition-all hover:border-transparent hover:drop-shadow-testimonial dark:border-dark-3 dark:bg-dark-2 dark:text-white dark:hover:drop-shadow-none"
+            onClick={handleNext}
+          >
+            {/* SVG Icon for Next */}
+            <svg
+              width="20"
+              height="21"
+              viewBox="0 0 20 21"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="fill-current"
+            >
+              <path
+                d="M18 9.6875L11.5312 3.125C11.25 2.84375 10.8125 2.84375 10.5312 3.125C10.25 3.40625 10.25 3.84375 10.5312 4.125L15.7812 9.46875H2.5C2.125 9.46875 1.8125 9.78125 1.8125 10.1562C1.8125 10.5312 2.125 10.875 2.5 10.875H15.8437L10.5312 16.2813C10.25 16.5625 10.25 17 10.5312 17.2813C10.6562 17.4063 10.8437 17.4688 11.0312 17.4688C11.2187 17.4688 11.4062 17.4062 11.5312 17.25L18 10.6875C18.2812 10.4062 18.2812 9.96875 18 9.6875Z"
+                fill=""
+              />
+            </svg>
+          </button>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
-
-export default Testimonial;
 
 const SingleTestimonial = ({
   image,
@@ -96,9 +91,13 @@ const SingleTestimonial = ({
   return (
     <div className="relative flex justify-center">
       <div className="relative w-full pb-16 md:w-11/12 lg:w-10/12 xl:w-8/12">
-        <div className="w-full items-center md:flex">
+        <div className="w-full flex flex-col md:flex-row md:items-center">
           <div className="relative mb-12 w-full max-w-[310px] md:mb-0 md:mr-12 md:max-w-[250px] lg:mr-14 lg:max-w-[280px] 2xl:mr-16">
-            <img src={image} alt="image" className="w-full" />
+            <img
+              src={image}
+              alt="testimonial"
+              className="w-full rounded-lg shadow-md"
+            />
             <span className="absolute -left-6 -top-6 z-[-1] hidden sm:block">
               <DotShape />
             </span>
@@ -121,13 +120,15 @@ const SingleTestimonial = ({
           <div className="w-full">
             <div>
               <div className="mb-7">
-                <img src={reviewImg} alt={reviewAlt} />
+                <img
+                  src={reviewImg}
+                  alt={reviewAlt}
+                  className="rounded-md shadow-sm"
+                />
               </div>
-
               <p className="mb-11 text-base font-normal italic leading-[1.81] text-body-color dark:text-dark-6 sm:text-[22px]">
                 {details}
               </p>
-
               <h4 className="mb-2 text-[22px] font-semibold leading-[27px] text-dark dark:text-white">
                 {name}
               </h4>
@@ -142,269 +143,16 @@ const SingleTestimonial = ({
   );
 };
 
-const DotShape = () => {
-  return (
-    <>
-      <svg
-        width="77"
-        height="77"
-        viewBox="0 0 77 77"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <circle
-          cx="1.66343"
-          cy="74.5221"
-          r="1.66343"
-          transform="rotate(-90 1.66343 74.5221)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="1.66343"
-          cy="30.94"
-          r="1.66343"
-          transform="rotate(-90 1.66343 30.94)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="16.3016"
-          cy="74.5221"
-          r="1.66343"
-          transform="rotate(-90 16.3016 74.5221)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="16.3016"
-          cy="30.94"
-          r="1.66343"
-          transform="rotate(-90 16.3016 30.94)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="30.9398"
-          cy="74.5221"
-          r="1.66343"
-          transform="rotate(-90 30.9398 74.5221)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="30.9398"
-          cy="30.94"
-          r="1.66343"
-          transform="rotate(-90 30.9398 30.94)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="45.578"
-          cy="74.5221"
-          r="1.66343"
-          transform="rotate(-90 45.578 74.5221)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="45.578"
-          cy="30.94"
-          r="1.66343"
-          transform="rotate(-90 45.578 30.94)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="60.2162"
-          cy="74.5216"
-          r="1.66343"
-          transform="rotate(-90 60.2162 74.5216)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="74.6634"
-          cy="74.5216"
-          r="1.66343"
-          transform="rotate(-90 74.6634 74.5216)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="60.2162"
-          cy="30.9398"
-          r="1.66343"
-          transform="rotate(-90 60.2162 30.9398)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="74.6634"
-          cy="30.9398"
-          r="1.66343"
-          transform="rotate(-90 74.6634 30.9398)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="1.66343"
-          cy="59.8839"
-          r="1.66343"
-          transform="rotate(-90 1.66343 59.8839)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="1.66343"
-          cy="16.3017"
-          r="1.66343"
-          transform="rotate(-90 1.66343 16.3017)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="16.3016"
-          cy="59.8839"
-          r="1.66343"
-          transform="rotate(-90 16.3016 59.8839)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="16.3016"
-          cy="16.3017"
-          r="1.66343"
-          transform="rotate(-90 16.3016 16.3017)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="30.9398"
-          cy="59.8839"
-          r="1.66343"
-          transform="rotate(-90 30.9398 59.8839)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="30.9398"
-          cy="16.3017"
-          r="1.66343"
-          transform="rotate(-90 30.9398 16.3017)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="45.578"
-          cy="59.8839"
-          r="1.66343"
-          transform="rotate(-90 45.578 59.8839)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="45.578"
-          cy="16.3017"
-          r="1.66343"
-          transform="rotate(-90 45.578 16.3017)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="60.2162"
-          cy="59.8839"
-          r="1.66343"
-          transform="rotate(-90 60.2162 59.8839)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="74.6634"
-          cy="59.8839"
-          r="1.66343"
-          transform="rotate(-90 74.6634 59.8839)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="60.2162"
-          cy="16.3017"
-          r="1.66343"
-          transform="rotate(-90 60.2162 16.3017)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="74.6634"
-          cy="16.3017"
-          r="1.66343"
-          transform="rotate(-90 74.6634 16.3017)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="1.66343"
-          cy="45.2455"
-          r="1.66343"
-          transform="rotate(-90 1.66343 45.2455)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="1.66343"
-          cy="1.66342"
-          r="1.66343"
-          transform="rotate(-90 1.66343 1.66342)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="16.3016"
-          cy="45.2455"
-          r="1.66343"
-          transform="rotate(-90 16.3016 45.2455)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="16.3016"
-          cy="1.66342"
-          r="1.66343"
-          transform="rotate(-90 16.3016 1.66342)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="30.9398"
-          cy="45.2455"
-          r="1.66343"
-          transform="rotate(-90 30.9398 45.2455)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="30.9398"
-          cy="1.66342"
-          r="1.66343"
-          transform="rotate(-90 30.9398 1.66342)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="45.578"
-          cy="45.2455"
-          r="1.66343"
-          transform="rotate(-90 45.578 45.2455)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="45.578"
-          cy="1.66344"
-          r="1.66343"
-          transform="rotate(-90 45.578 1.66344)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="60.2162"
-          cy="45.2458"
-          r="1.66343"
-          transform="rotate(-90 60.2162 45.2458)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="74.6634"
-          cy="45.2458"
-          r="1.66343"
-          transform="rotate(-90 74.6634 45.2458)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="60.2162"
-          cy="1.66371"
-          r="1.66343"
-          transform="rotate(-90 60.2162 1.66371)"
-          fill="#3056D3"
-        />
-        <circle
-          cx="74.6634"
-          cy="1.66371"
-          r="1.66343"
-          transform="rotate(-90 74.6634 1.66371)"
-          fill="#3056D3"
-        />
-      </svg>
-    </>
-  );
-};
+const DotShape = () => (
+  <svg
+    width="77"
+    height="77"
+    viewBox="0 0 77 77"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="38.5" cy="38.5" r="38.5" fill="#F6F8FB" />
+  </svg>
+);
+
+export default Testimonial;
