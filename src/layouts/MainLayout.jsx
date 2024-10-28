a88e3d085f518cf5faee763af0728e9b12c81490
piseth sattya <pisethsattya33@gmail.com>
2024-10-28 11:50:45 +0700

    testing

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -22,7 +22,7 @@ export default function MainLayout() {
     <>
       <StickyBar />
       <Navbar />
-      <main className="bg-gray-50">
+      <main className="bg-gray-50 overflow-hidden">
         <section>
           <Outlet />
         </section>
