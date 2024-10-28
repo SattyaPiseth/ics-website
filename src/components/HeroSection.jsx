e2b9cd73fd406b1be4a2ff355f1afab15ea7e7d9
piseth sattya <pisethsattya33@gmail.com>
2024-10-28 11:03:04 +0700

    testing

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,10 +7,16 @@ const HeroSection = () => {
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4 lg:w-5/12">
             <div className="hero-content">
-              <h1 className="mb-5 text-4xl font-bold !leading-[1.208] text-dark dark:text-white sm:text-[42px] lg:text-[40px] xl:text-5xl" data-aos="fade-up">
+              <h1
+                className="mb-5 text-4xl font-bold !leading-[1.208] text-dark dark:text-white sm:text-[42px] lg:text-[40px] xl:text-5xl"
+                data-aos="fade-up"
+              >
                 {heroData.title}
               </h1>
-              <p className="mb-8 max-w-[480px] text-base lg:text-lg text-dark dark:text-gray-400" data-aos="fade-up">
+              <p
+                className="mb-8 max-w-[480px] text-base lg:text-lg text-dark dark:text-gray-400"
+                data-aos="fade-up"
+              >
                 {heroData.description}
               </p>
               <ul className="flex flex-wrap items-center" data-aos="fade-right">
@@ -29,7 +35,12 @@ const HeroSection = () => {
                             fill="none"
                             xmlns="http://www.w3.org/2000/svg"
                           >
-                            <circle cx="12" cy="12.6152" r="12" fill="#0070C0" />
+                            <circle
+                              cx="12"
+                              cy="12.6152"
+                              r="12"
+                              fill="#0070C0"
+                            />
                             <rect
                               x="7.99893"
                               y="14.979"
@@ -61,9 +72,17 @@ const HeroSection = () => {
                   Some Of Our Donors
                   <span className="ml-3 inline-block h-px w-8 bg-dark dark:bg-gray-400"></span>
                 </h6>
-                <div className="flex items-center space-x-4" data-aos="fade-left">
+                <div
+                  className="flex items-center space-x-4"
+                  data-aos="fade-left"
+                >
                   {heroData.clients.map((client, index) => (
-                    <SingleImage key={index} href={client.href} imgSrc={client.imgSrc} alt={client.alt} />
+                    <SingleImage
+                      key={index}
+                      href={client.href}
+                      imgSrc={client.imgSrc}
+                      alt={client.alt}
+                    />
                   ))}
                 </div>
               </div>
@@ -72,48 +91,49 @@ const HeroSection = () => {
           <div className="hidden px-4 lg:block lg:w-1/12"></div>
           <div className="w-full px-4 lg:w-6/12">
             <div className="lg:ml-auto lg:text-right">
-              <div className="relative z-10 inline-block pt-11 lg:pt-0" data-aos="fade-left">
+              <div className="relative z-10 inline-block pt-11 lg:pt-0">
                 <img
                   src={heroData.imageSrc}
                   alt="hero"
                   className="max-w-full lg:ml-auto rounded-lg"
+                  data-aos="flip-left"
                 />
                 {/* SVG Background */}
                 <span className="absolute -bottom-8 -left-8 z-[-1]">
                   {/* SVG circles here */}
                   <svg
-                      width="93"
-                      height="93"
-                      viewBox="0 0 93 93"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <circle cx="2.5" cy="2.5" r="2.5" fill="#0070C0" />
-                      <circle cx="2.5" cy="24.5" r="2.5" fill="#0070C0" />
-                      <circle cx="2.5" cy="46.5" r="2.5" fill="#0070C0" />
-                      <circle cx="2.5" cy="68.5" r="2.5" fill="#0070C0" />
-                      <circle cx="2.5" cy="90.5" r="2.5" fill="#0070C0" />
-                      <circle cx="24.5" cy="2.5" r="2.5" fill="#0070C0" />
-                      <circle cx="24.5" cy="24.5" r="2.5" fill="#0070C0" />
-                      <circle cx="24.5" cy="46.5" r="2.5" fill="#0070C0" />
-                      <circle cx="24.5" cy="68.5" r="2.5" fill="#0070C0" />
-                      <circle cx="24.5" cy="90.5" r="2.5" fill="#0070C0" />
-                      <circle cx="46.5" cy="2.5" r="2.5" fill="#0070C0" />
-                      <circle cx="46.5" cy="24.5" r="2.5" fill="#0070C0" />
-                      <circle cx="46.5" cy="46.5" r="2.5" fill="#0070C0" />
-                      <circle cx="46.5" cy="68.5" r="2.5" fill="#0070C0" />
-                      <circle cx="46.5" cy="90.5" r="2.5" fill="#0070C0" />
-                      <circle cx="68.5" cy="2.5" r="2.5" fill="#0070C0" />
-                      <circle cx="68.5" cy="24.5" r="2.5" fill="#0070C0" />
-                      <circle cx="68.5" cy="46.5" r="2.5" fill="#0070C0" />
-                      <circle cx="68.5" cy="68.5" r="2.5" fill="#0070C0" />
-                      <circle cx="68.5" cy="90.5" r="2.5" fill="#0070C0" />
-                      <circle cx="90.5" cy="2.5" r="2.5" fill="#0070C0" />
-                      <circle cx="90.5" cy="24.5" r="2.5" fill="#0070C0" />
-                      <circle cx="90.5" cy="46.5" r="2.5" fill="#0070C0" />
-                      <circle cx="90.5" cy="68.5" r="2.5" fill="#0070C0" />
-                      <circle cx="90.5" cy="90.5" r="2.5" fill="#0070C0" />
-                    </svg>
+                    width="93"
+                    height="93"
+                    viewBox="0 0 93 93"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle cx="2.5" cy="2.5" r="2.5" fill="#0070C0" />
+                    <circle cx="2.5" cy="24.5" r="2.5" fill="#0070C0" />
+                    <circle cx="2.5" cy="46.5" r="2.5" fill="#0070C0" />
+                    <circle cx="2.5" cy="68.5" r="2.5" fill="#0070C0" />
+                    <circle cx="2.5" cy="90.5" r="2.5" fill="#0070C0" />
+                    <circle cx="24.5" cy="2.5" r="2.5" fill="#0070C0" />
+                    <circle cx="24.5" cy="24.5" r="2.5" fill="#0070C0" />
+                    <circle cx="24.5" cy="46.5" r="2.5" fill="#0070C0" />
+                    <circle cx="24.5" cy="68.5" r="2.5" fill="#0070C0" />
+                    <circle cx="24.5" cy="90.5" r="2.5" fill="#0070C0" />
+                    <circle cx="46.5" cy="2.5" r="2.5" fill="#0070C0" />
+                    <circle cx="46.5" cy="24.5" r="2.5" fill="#0070C0" />
+                    <circle cx="46.5" cy="46.5" r="2.5" fill="#0070C0" />
+                    <circle cx="46.5" cy="68.5" r="2.5" fill="#0070C0" />
+                    <circle cx="46.5" cy="90.5" r="2.5" fill="#0070C0" />
+                    <circle cx="68.5" cy="2.5" r="2.5" fill="#0070C0" />
+                    <circle cx="68.5" cy="24.5" r="2.5" fill="#0070C0" />
+                    <circle cx="68.5" cy="46.5" r="2.5" fill="#0070C0" />
+                    <circle cx="68.5" cy="68.5" r="2.5" fill="#0070C0" />
+                    <circle cx="68.5" cy="90.5" r="2.5" fill="#0070C0" />
+                    <circle cx="90.5" cy="2.5" r="2.5" fill="#0070C0" />
+                    <circle cx="90.5" cy="24.5" r="2.5" fill="#0070C0" />
+                    <circle cx="90.5" cy="46.5" r="2.5" fill="#0070C0" />
+                    <circle cx="90.5" cy="68.5" r="2.5" fill="#0070C0" />
+                    <circle cx="90.5" cy="90.5" r="2.5" fill="#0070C0" />
+                  </svg>
                 </span>
               </div>
             </div>
