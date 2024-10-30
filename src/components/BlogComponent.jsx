9d76c8900732b881b0e6437fb712a8946a998d7e
piseth sattya <pisethsattya33@gmail.com>
2024-10-30 09:43:33 +0700

    testing

--- src/components/BlogComponent.jsx
+++ src/components/BlogComponent.jsx
@@ -48,9 +48,8 @@ export default BlogComponent;
 const BlogCard = ({ image, date, CardTitle, CardDescription }) => {
   return (
     <div
-      className="w-full px-4 md:w-1/2 lg:w-1/3"
+      className="w-full px-4 md:w-1/3 lg:w-1/5"
       data-aos="fade-up"
-      data-aos-anchor-placement="top-bottom"
     >
       <div className="mb-10 w-full">
         <div className="mb-8 overflow-hidden rounded">
