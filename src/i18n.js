4f52962b22b44407636fd931e39d68c5af4fdff9
piseth sattya <pisethsattya33@gmail.com>
2025-01-11 14:03:48 +0700

    testing

--- src/i18n.js
+++ src/i18n.js
@@ -9,7 +9,7 @@ i18n
     .use(initReactI18next) // pass the i18n instance to react-i18next module
     .init({
         backend: {
-            loadPath: 'src/locales/{{lng}}/{{ns}}.json', // path where resources are loaded from
+            loadPath: './src/locales/{{lng}}/{{ns}}.json', // path where resources are loaded from
         },
         lng: savedLanguage,
         fallbackLng: 'kh',
