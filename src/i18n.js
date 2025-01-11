47813e5ecce1b12ffef7cb2ecb0db1890ddcf18b
piseth sattya <pisethsattya33@gmail.com>
2025-01-11 14:01:33 +0700

    testing

--- src/i18n.js
+++ src/i18n.js
@@ -1,30 +1,33 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import enTranslation from "./locales/en/translation.json";
-import khTranslation from "./locales/kh/translation.json";
+import HttpBackend from 'i18next-http-backend';
 
 const savedLanguage = localStorage.getItem('i18nextLng') || 'kh';
 
 i18n
-    .use(initReactI18next)
+    .use(HttpBackend) // use the HttpBackend
+    .use(initReactI18next) // pass the i18n instance to react-i18next module
     .init({
-        resources: {
-            en: {
-                translation: enTranslation,
-            },
-            kh: {
-                translation: khTranslation,
-            },
+        backend: {
+            loadPath: 'src/locales/{{lng}}/{{ns}}.json', // path where resources are loaded from
         },
         lng: savedLanguage,
         fallbackLng: 'kh',
+        ns: ['translation'], // you can list all your namespaces here if multiple
+        defaultNS: 'translation',
         interpolation: {
-            escapeValue: false
+            escapeValue: false // not needed for react as it escapes by default
         },
     });
 
-    i18n.on('languageChanged',(lng)=>{
-        localStorage.setItem('i18nextLng',lng);
-    });
+i18n.on('languageChanged', (lng) => {
+    localStorage.setItem('i18nextLng', lng); // save the new language preference
+});
+
+// Error handling for loading translation files
+i18n.on('failedLoading', (lng, ns, msg) => {
+    console.error(`Failed loading: ${ns} for language: ${lng}, Message: ${msg}`);
+    // Optionally, implement additional logic such as notifications to users, retries, or fallbacks.
+});
 
 export default i18n;
