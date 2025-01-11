069650bededa6238c5d3b0491edfbf8dd7086fdf
piseth sattya <pisethsattya33@gmail.com>
2025-01-11 14:09:57 +0700

    testing

--- src/i18n.js
+++ src/i18n.js
@@ -1,24 +1,25 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 import HttpBackend from 'i18next-http-backend';
 
 const savedLanguage = localStorage.getItem('i18nextLng') || 'kh';
 
 i18n
-    .use(HttpBackend) // use the HttpBackend
-    .use(initReactI18next) // pass the i18n instance to react-i18next module
-    .init({
-        backend: {
-            loadPath: 'locales/{{lng}}/{{ns}}.json', // path where resources are loaded from
-        },
-        lng: savedLanguage,
-        fallbackLng: 'kh',
-        ns: ['translation'], // you can list all your namespaces here if multiple
-        defaultNS: 'translation',
-        interpolation: {
-            escapeValue: false // not needed for react as it escapes by default
-        },
-    });
+  .use(HttpBackend) // use the HttpBackend
+  .use(initReactI18next) // pass the i18n instance to react-i18next module
+  .init({
+      backend: {
+          // Correcting the path to ensure it points to the public directory
+          loadPath: '/locales/{{lng}}/{{ns}}.json', // Assuming public/locales/ structure
+      },
+      lng: savedLanguage,
+      fallbackLng: 'kh',
+      ns: ['translation'], // namespaces
+      defaultNS: 'translation',
+      interpolation: {
+          escapeValue: false // not needed for React as it escapes by default
+      },
+  });
 
 i18n.on('languageChanged', (lng) => {
     localStorage.setItem('i18nextLng', lng); // save the new language preference
