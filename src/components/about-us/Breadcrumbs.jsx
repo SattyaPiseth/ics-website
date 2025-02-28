5d2279a90b9ea8fe8e6d334fe97de00e5cd50066
piseth sattya <pisethsattya33@gmail.com>
2025-02-28 16:39:27 +0700

    testing

--- src/components/about-us/Breadcrumbs.jsx
+++ src/components/about-us/Breadcrumbs.jsx
@@ -15,7 +15,7 @@ const Breadcrumb = () => {
   return (
     <div className="py-10 bg-white dark:bg-dark">
       <div className="container">
-        <div className="w-full mb-8">
+        <div className="w-full">
           <div className="px-4 py-4 bg-white border rounded-lg border-light dark:bg-dark-2 dark:border-dark-3 shadow-1 dark:shadow-card sm:px-6 md:px-8 md:py-5">
             <ul className="flex items-center">
               <li>
