3945b6d4bedec7d6a45ddc1c39316b489b1a9a4d
piseth sattya <pisethsattya33@gmail.com>
2025-03-10 14:09:17 +0700

    testing

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -27,13 +27,13 @@ export default function NavbarComponent() {
                   src={ics_logo}
                   alt="ICS Skillful Parenting Logo"
                   className="w-44 sm:w-52 md:w-60 xl:w-80 transition-all duration-300 dark:hidden"
-                  loading="lazy"
+                  loading="eager"
                 />
                 <img
                   src={ics_logo}
                   alt="ICS Skillful Parenting Logo (Dark Mode)"
                   className="hidden dark:block w-44 sm:w-52 md:w-60 xl:w-80 transition-all duration-300"
-                  loading="lazy"
+                  loading="eager"
                 />
               </NavLink>
             </div>
